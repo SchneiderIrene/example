d21d834a4ed6911243d9dd9222e066c454c25e33
SchneiderIrene <schneider_irene@outlook.com>
2023-07-30 15:36:27 +0400

    implement mud function

--- src/main/java/com/schneiderirene/git/exaple/App.java
+++ src/main/java/com/schneiderirene/git/exaple/App.java
@@ -2,31 +2,33 @@ package com.schneiderirene.git.exaple;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
 
         int a = 5;
         int b = 3;
-        System.out.println(add(a,b));
-        System.out.println(sub(a,b));
-        System.out.println(mod(a,b));
-    }
-    private static int add (int a, int b){
-        return a+b;
+        System.out.println(add(a, b));
+        System.out.println(sub(a, b));
+        System.out.println(mod(a, b));
+        System.out.println(mud(a, b));
     }
 
-    private static int sub (int a, int b){
-        return a-b;
-    }
-    private static int mod (int a, int b){
-        return a%b;
+    private static int add(int a, int b) {
+        return a + b;
     }
 
+    private static int sub(int a, int b) {
+        return a - b;
+    }
 
+    private static int mod(int a, int b) {
+        return a % b;
+    }
+
+    private static int mud(int a, int b) {
+        return a * b;
+    }
 
 
 }

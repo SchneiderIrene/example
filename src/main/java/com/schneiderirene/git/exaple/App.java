3d52c845cf198b82b8b58ba3573d56fabed393bf
SchneiderIrene <schneider_irene@outlook.com>
2023-07-30 14:02:14 +0400

    implement sub function

--- src/main/java/com/schneiderirene/git/exaple/App.java
+++ src/main/java/com/schneiderirene/git/exaple/App.java
@@ -12,9 +12,13 @@ public class App
         int a = 5;
         int b = 3;
         System.out.println(add(a,b));
+        System.out.println(sub(a,b));
     }
     private static int add (int a, int b){
         return a+b;
     }
+
+    private static int sub (int a, int b){
+        return a-b;}
 
 }

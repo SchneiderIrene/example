c626554785bd0c67f665948c22e59b0d1c406c22
SchneiderIrene <schneider_irene@outlook.com>
2023-07-30 14:00:19 +0400

    implement add function

--- src/main/java/com/schneiderirene/git/exaple/App.java
+++ src/main/java/com/schneiderirene/git/exaple/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+        int a = 5;
+        int b = 3;
+        System.out.println(add(a,b));
     }
+    private static int add (int a, int b){
+        return a+b;
+    }
+
 }
